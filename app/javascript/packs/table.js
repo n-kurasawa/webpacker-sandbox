d141826c92b124ae39114c790880a1f312fb1bfd
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2018-05-04 11:21:18 +0900

    try css

--- app/javascript/packs/table.js
+++ app/javascript/packs/table.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import ReactTable from 'react-table';
+// import "react-table/react-table.css";
+import '../table/styles/table.css'
 
 export default class Table extends React.Component {
   render() {
