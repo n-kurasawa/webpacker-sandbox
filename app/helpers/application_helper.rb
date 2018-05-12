f344d754f025d87155bd8f4d6975e0b858bbd07f
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2018-05-12 20:03:50 +0900

    rename tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def javascript_tag(js, id, data)
+  def javascript_tag_with_data(js, id, data)
     content_tag(:div, '', id: id, data: { json: data.to_json }) do
       javascript_pack_tag js
     end
