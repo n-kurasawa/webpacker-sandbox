9446d5f603f7a1129452c4fb9cc7803b46a7feab
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2018-05-12 19:19:48 +0900

    add class

--- app/javascript/components/table.js
+++ app/javascript/components/table.js
@@ -1,29 +1,32 @@
 import React from 'react';
 
-export default ({headers, rows, sort}) =>
+export default ({headers, rows, sort, order}) =>
   <table border="1">
-    <Header headers={headers} sort={sort} />
+    <Header headers={headers} sort={sort} order={order}/>
     <Body rows={rows} />
   </table>;
 
-const Header = ({headers, sort}) =>
+const Header = ({headers, sort, order}) =>
   <thead>
-  <tr>
-    {
-      headers.map((header, i) =>
-        <th key={i} onClick={() => sort(header)}>{header}</th>
-      )
-    }
-  </tr>
+    <tr>
+      {
+        headers.map((header, i) => {
+          if (header === order.key) {
+            return <th className={order.by} key={i} onClick={() => sort(header)}>{header}</th>
+          }
+          return <th key={i} onClick={() => sort(header)}>{header}</th>
+        })
+      }
+    </tr>
   </thead>;
 
 const Body = ({rows}) =>
   <tbody>
-  {
-    rows.map((row, i) =>
-      <tr key={i}>
-        { row.map((col, j) => <td key={j}>{col}</td>) }
-      </tr>
-    )
-  }
+    {
+      rows.map((row, i) =>
+        <tr key={i}>
+          { row.map((col, j) => <td key={j}>{col}</td>) }
+        </tr>
+      )
+    }
   </tbody>;
--- app/javascript/packs/original_table.js
+++ app/javascript/packs/original_table.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import TableContainer from '../containers/table-container';
 
-
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <TableContainer headers={['name', 'age', 'other']} rows={[['倉澤', '29', 'hoge'], ['テスト', '2', 'test'], ['kkk', '100', 'abc']]} defaultSort={0}/>,
+    <TableContainer headers={['name', 'age', 'other']} rows={[['倉澤', '29', 'hoge'], ['テスト', '2', 'test'], ['kkk', '100', 'abc']]} defaultSort={1}/>,
     document.body.appendChild(document.createElement('div')),
   )
 });
