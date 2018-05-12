586aed0a0c1714a20a417227f6dd8c053fbc0d07
n-kurasawa <kurasawa.nk.naohiro@gmail.com>
2018-05-12 18:01:16 +0900

    sort asc

--- app/javascript/packs/original_table.js
+++ app/javascript/packs/original_table.js
@@ -1,37 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-const OriginalTable = ({headers, rows}) =>
-  <table>
-    <Header headers={headers} />
-    <Body rows={rows} />
-  </table>;
-
-const Header = ({headers}) =>
-  <thead>
-  <tr>
-    {
-      headers.map((header) =>
-        <th>{header}</th>
-      )
+class OriginalTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: props.rows
     }
-  </tr>
+  }
+
+  sort(key) {
+    const i = this.props.headers.indexOf(key);
+    if (i === -1) { return; }
+
+    const rows = this.state.rows.sort((before, after) => {
+      const n = before[i] - after[i];
+      if (!isNaN(n)) {
+        return n;
+      }
+      if (before[i] < after[i]) {
+        return -1;
+      }
+      if (before[i] > after[i]) {
+        return 1;
+      }
+      return 0;
+    });
+    this.setState({rows: rows});
+  }
+
+  render() {
+    return (
+      <table border="1">
+        <Header headers={this.props.headers} sort={this.sort.bind(this)}/>
+        <Body rows={this.state.rows} />
+      </table>
+    );
+  }
+}
+
+const Header = ({headers, sort}) =>
+  <thead>
+    <tr>
+      {
+        headers.map((header, i) =>
+          <th key={i} onClick={() => sort(header)}>{header}</th>
+        )
+      }
+    </tr>
   </thead>;
 
 const Body = ({rows}) =>
   <tbody>
-  {
-    rows.map((row) =>
-      <tr>
-        { row.map((col) => <td>{col}</td>) }
-      </tr>
-    )
-  }
+    {
+      rows.map((row, i) =>
+        <tr key={i}>
+          { row.map((col, j) => <td key={`${j}`}>{col}</td>) }
+        </tr>
+      )
+    }
   </tbody>;
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <OriginalTable headers={['name', 'age', 'other']} rows={[['倉澤', '29', 'hoge'], ['テスト', '32', 'test']]} />,
+    <OriginalTable headers={['name', 'age', 'other']} rows={[['倉澤', '29', 'hoge'], ['テスト', '2', 'test'], ['kkk', '100', 'abc']]} />,
     document.body.appendChild(document.createElement('div')),
   )
 });
